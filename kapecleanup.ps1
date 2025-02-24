f0260685c30410d19568a41c54b0aa99b8f0ab78
Derek Armstrong <derek.v.armstrong@gmail.com>
2025-02-24 11:32:51 -0700

    Update kapecleanup.ps1

--- kapecleanup.ps1
+++ kapecleanup.ps1
@@ -3,6 +3,12 @@
 $zipFilePath = "C:\ProgramData\Microsoft\Windows Defender Advanced Threat Protection\Downloads\kape.zip"
 $extractPath = "C:\kape-da"
 
-# Clean up files
-Remove-Item $zipFilePath
-Remove-Item $extractPath -Recurse
+# Check if the extraction directory exists, if exists, delete it
+if (Test-Path -Path $extractPath -PathType Container) {
+    Remove-Item $extractPath -Recurse
+}
+
+# Check if the zip file exists, if exists, delete it
+if (Test-Path $zipFilePath) {
+   Remove-Item $zipFilePath
+}
